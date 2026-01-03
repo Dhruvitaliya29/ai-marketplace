23cbc8332dd4f4c7085e386577078cb173b9bd54
Dhruvitaliya29 <138241167+Dhruvitaliya29@users.noreply.github.com>
2026-01-03 12:40:43 +0530

    Update app.py

--- sentiment/app.py
+++ sentiment/app.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Input(BaseModel):
+    text: str
 
 @app.get("/")
 def root():
     return {"status": "Sentiment service running"}
+
+@app.post("/infer")
+def infer(data: Input):
+    text = data.text.lower()
+
+    if "good" in text or "great" in text or "excellent" in text:
+        sentiment = "positive"
+    elif "bad" in text or "poor" in text or "worst" in text:
+        sentiment = "negative"
+    else:
+        sentiment = "neutral"
+
+    return {
+        "sentiment": sentiment
+    }
